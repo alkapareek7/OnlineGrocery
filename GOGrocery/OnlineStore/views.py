525b4b6c221cbed5825bc0fd7975c8c81694abde
alka <alkapareek7@gamil.com>
2023-03-26 10:47:29 +0300

    printing Hello world

--- GOGrocery/OnlineStore/views.py
+++ GOGrocery/OnlineStore/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
 
 # Create your views here.
 from django.shortcuts import render, redirect
 from .models import Store
 # Create your views here.
 def Store(request):
-        return "Hello world"
+   return HttpResponse("Hello world!")
