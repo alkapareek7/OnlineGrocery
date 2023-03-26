5d218f800d873f2e53a099532a7196833078ce14
alka <alkapareek7@gamil.com>
2023-03-26 10:40:54 +0300

    making app

--- GOGrocery/OnlineStore/models.py
+++ GOGrocery/OnlineStore/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.IntegerField(default= False)
--- GOGrocery/OnlineStore/views.py
+++ GOGrocery/OnlineStore/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import Store
+# Create your views here.
+def Store(request):
+        return "Hello world"
