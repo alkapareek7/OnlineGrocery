6b226320b9ebc0ce4571eeb002b84e61a7f259ba
Anni Peipsi <pentanni@hotmail.com>
2023-03-28 20:21:52 +0300

    added login and register

--- GOGrocery/GOGrocery/urls.py
+++ GOGrocery/GOGrocery/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('OnlineStore.urls'))
+    path('', include('OnlineStore.urls')),
+    path('accounts/' , include('accounts.urls'))
 ]
---
+++ GOGrocery/accounts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        user = User.object.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+        user.save();
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
+    
+def login(request):
+    return render(request, 'login.html')
