be48a4cae39f7052e775edf2f640d1a200c89d83
Anni Peipsi <pentanni@hotmail.com>
2023-03-30 10:28:07 +0300

    Some changes in login and register

--- GOGrocery/accounts/views.py
+++ GOGrocery/accounts/views.py
@@ -1,22 +1,36 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
-def register(request):
+def registerPage(request):
     if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
 
-        user = User.object.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+        user = User.objects.create_user(username=username, password=password1, email=email)
         user.save();
-        print('user created')
-        return redirect('/')
+        
+        return redirect('login')
     else:
         return render(request,'register.html')
     
-def login(request):
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        User = authenticate(request, username=username, password=password)
+
+        if User is not None:
+            login(request, User)
+            return redirect('home')
+
+
+    context = {}
     return render(request, 'login.html')
+
+
