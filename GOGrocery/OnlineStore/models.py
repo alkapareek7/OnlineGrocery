4f3309cb25a017fdfb6e395c0a6a38aa96776ea8
alka <alkapareek7@gamil.com>
2023-03-30 09:09:33 +0300

    "changes in webpage"

--- GOGrocery/OnlineStore/models.py
+++ GOGrocery/OnlineStore/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.contrib.auth.decorators import login_required
 
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField(default= False)
+
+@login_required
+def my_view(request):
+    pass
--- GOGrocery/OnlineStore/views.py
+++ GOGrocery/OnlineStore/views.py
@@ -4,6 +4,31 @@ from django.http import HttpResponse
 # Create your views here.
 from django.shortcuts import render, redirect
 from .models import Store
+from django.contrib.auth.views import LoginView
+from django.views.generic.base import TemplateView
+
+
+
+from django.urls import reverse_lazy
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     return render (request, "home.html")
+
+
+class MyLoginView(LoginView):
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks') 
+    
+    def form_invalid(self, form):
+        messages.error(self.request,'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
+    
+
+
+
+class MyLogOutView(TemplateView):
+    template_name = "registration/logged_out.html"
