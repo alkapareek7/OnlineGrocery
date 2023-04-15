76136046df5b0c5552a9d0e13f7f36b3d5509d70
Käthlin <k2thlins@gmail.com>
2023-04-15 15:09:20 +0300

    created class GroceryList and RecipeLists

--- GOGrocery/OnlineStore/models.py
+++ GOGrocery/OnlineStore/models.py
@@ -5,14 +5,72 @@ from django.db.models import (
 CASCADE, CharField, ForeignKey, Model, BooleanField, DateField, IntegerField)
 from datetime import datetime
 
+from django.urls import reverse
+
 # Create your models here.
+
+class ItemsAdmin(ModelAdmin):
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description=None)
+
+    ordering = ['id']
+    list_display = ['id', 'name','grocery_list', 'description', 'is_completed']
+    list_display_links = ['id', 'name','description']
+    list_per_page = 30
+    list_filter = ['description']
+    search_fields = ['name']
+    actions = ['cleanup_description']
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        (
+            'Link to Another table',
+            {
+                'fields': ['grocery_list'],
+                'description': (
+                    'Select form the list of possible lists.'
+                )
+            }
+        ),
+        (
+            'Item Information',
+            {
+                'fields': ['description'],
+                'description': 'These fields are intended to be filled in by our users.'
+            }
+        )
+    ]
+    readonly_fields = ['is_completed']
+
+
 class Store(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField(max_length=20)
 
-class groceryList(Model):
-    Vegetable_name = CharField('Shopping list name', max_length=100)
+class GroceryList(Model):
+    Vegetable_name = CharField('Shopping list item name', max_length=100)
     user = ForeignKey(User, on_delete = CASCADE)
     created_at = DateField(default=datetime.now())
     is_organic = BooleanField(default=False)
     quantity = IntegerField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class RecipeList(Model):
+    name= CharField("Grocery list item name", max_length=100)
+    description = CharField("Shopping list item description", max_length=300, null=True, blank=True)
+    is_completed = BooleanField(default=False)
+    grocery_list = ForeignKey(GroceryList, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('items-list', kwargs={'pk': self.grocery_list.pk})
+
--- GOGrocery/OnlineStore/views.py
+++ GOGrocery/OnlineStore/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from .models import Store
+from .models import GroceryList, RecipeList
 from django.contrib.auth.views import LoginView
 from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User
@@ -16,11 +16,52 @@ def home(request):
     return render (request, "home.html")
 
 def groceryList(request):
-    return render (request, "groceryList.html")
 
-def reciepeLists(request):
-    return render (request, "reciepeLists.html")
+    if request.method == "GET":
+        grocery_lists = GroceryList.objects.filter(user=request.user)
+        return render(request, "groceryList.html", context={"grocery_lists": grocery_lists})
     
+    if request.method == "POST":
+        if request.POST.get("recipe_add"):
+            checked = False
+            if request.POST.get("checkbox"):
+                checked = True
+
+            item_name = request.POST.get("recipe") 
+            GroceryList.objects.create(title = item_name, user = request.user, is_public=checked)
+        elif request.POST.get("delete"):
+            id_to_delete = request.POST.get("delete")
+            item_to_delete = GroceryList.objects.get(pk=id_to_delete)
+            item_to_delete.delete()
+        return redirect("lists-list")
+    
+
+def reciepeLists(request, pk):
+    shopping_list = RecipeList.objects.get(pk=pk)
+    items_list = shopping_list.shoppingitem_set.all()
+
+    if request.method == "GET":
+        return render(request, "reciepeLists.html", context={"shopping_list": items_list, "header" : shopping_list})
+    
+    if request.method == "POST":
+        if request.POST.get("item_add"):
+            item_name = request.POST.get("item_name")
+            item_description = request.POST.get("item_description")
+
+            shopping_list.shoppingitem_set.create(name=item_name, description=item_description)
+
+        elif request.POST.get("update_checked"):
+            checked = request.POST.getlist("checkbox")
+            for item in items_list:
+                item.is_completed = True if str(item.pk) in checked else False
+                item.save()
+        elif request.POST.get("delete"):
+            id_to_delete = request.POST.get("delete")
+            item_to_delete = shopping_list.shoppingitem_set.get(pk=id_to_delete)
+            item_to_delete.delete()
+
+        return redirect("items-list", pk)
+        
 
 def registerPage(request):
     if request.method == 'POST':
