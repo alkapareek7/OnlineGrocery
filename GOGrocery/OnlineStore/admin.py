ad545c33be8160fc5fc7a4cfe6af1c194dcbb538
alka <alkapareek7@gamil.com>
2023-04-16 13:31:38 +0300

    "changes  in final HTML"

--- GOGrocery/OnlineStore/admin.py
+++ GOGrocery/OnlineStore/admin.py
@@ -2,17 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin 
-<<<<<<< HEAD
 from .models import Store, groceryList, GroceryItems
 # Register your models here.
 
 admin.site.register(Store)
 admin.site.register(groceryList)
 admin.site.register(GroceryItems)
-=======
-from .models import Store, GroceryList
 
-# Register your models here.
 
 admin.site.register(Store)
 admin.site.register(GroceryList)
--- GOGrocery/OnlineStore/models.py
+++ GOGrocery/OnlineStore/models.py
@@ -5,49 +5,7 @@ from django.db.models import (
 CASCADE, CharField, ForeignKey, Model, BooleanField, DateField, IntegerField)
 from datetime import datetime
 
-from django.urls import reverse
-
 # Create your models here.
-
-class ItemsAdmin(ModelAdmin):
-
-    @staticmethod
-    def released_year(obj):
-        return obj.released.year
-
-    @staticmethod
-    def cleanup_description(modeladmin, request, queryset):
-        queryset.update(description=None)
-
-    ordering = ['id']
-    list_display = ['id', 'name','grocery_list', 'description', 'is_completed']
-    list_display_links = ['id', 'name','description']
-    list_per_page = 30
-    list_filter = ['description']
-    search_fields = ['name']
-    actions = ['cleanup_description']
-    fieldsets = [
-        (None, {'fields': ['name']}),
-        (
-            'Link to Another table',
-            {
-                'fields': ['grocery_list'],
-                'description': (
-                    'Select form the list of possible lists.'
-                )
-            }
-        ),
-        (
-            'Item Information',
-            {
-                'fields': ['description'],
-                'description': 'These fields are intended to be filled in by our users.'
-            }
-        )
-    ]
-    readonly_fields = ['is_completed']
-
-
 class Store(models.Model):
     name = models.CharField(max_length=20)
     quantity = models.IntegerField(max_length=20)
@@ -59,7 +17,6 @@ class GroceryList(Model):
     is_organic = BooleanField(default=False)
     quantity = IntegerField(max_length=100)
 
-<<<<<<< HEAD
 class GroceryItems(Model):
     name = CharField('Shopping list name', max_length=100)
     is_organic = BooleanField(default=False)
@@ -101,20 +58,3 @@ class ItemsAdmin(ModelAdmin):
         )
   ]
 readonly_fields = ['is_completed']
-=======
-    def __str__(self):
-        return self.title
-
-class RecipeList(Model):
-    name= CharField("Grocery list item name", max_length=100)
-    description = CharField("Shopping list item description", max_length=300, null=True, blank=True)
-    is_completed = BooleanField(default=False)
-    grocery_list = ForeignKey(GroceryList, on_delete=CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('items-list', kwargs={'pk': self.grocery_list.pk})
-
->>>>>>> c15f5164e3ebff596aadd9ec4df778d378b10fb9
